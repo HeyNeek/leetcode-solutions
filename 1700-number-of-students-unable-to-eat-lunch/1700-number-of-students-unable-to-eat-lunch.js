157a030a3e886f3084592f6316fe3d61094bdc73
HeyNeek <45445231+HeyNeek@users.noreply.github.com>
2022-05-16 00:58:18 -0500

    Time: 108 ms (22.02%), Space: 43.3 MB (17.26%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ */
+var countStudents = function(students, sandwiches) {
+    let count = 0;
+    console.log(count);
+    
+    while(students.length > 0 && sandwiches.length > 0 && count < 1000){
+        if(students[0] === sandwiches[0]){
+            students.shift();
+            sandwiches.shift();
+            count++;
+        }else{
+            students.push(students[0]);
+            students.shift();
+            count++;
+        }
+    }
+    
+    console.log(count);
+    console.log(students);
+    console.log(sandwiches);
+    
+    if(students.length > 0){
+        return students.length;
+    }else{
+        return 0;
+    }
+    
+};
